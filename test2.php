bad2b26491861ebff7d63756a59d1a3c93bfcec2
Khalifa Alremeithi <kalremei@purdue.edu>
2019-02-08 15:22:30 -0500

    d

--- test2.php
+++ test2.php
@@ -6,16 +6,5 @@
  * Time: 2:55 PM
  */
 
-$url = 'http://admin.scaninsystem.com/backend/loginValidator.php';
-$data = array('email' => 'lee2043@purdue.edu', 'password' => 'eugene123');
-$options = array(
-    'http' => array(
-        'header' => "Content-type: application/x-www-form-urlencoded\r\n",
-        'method' => 'POST',
-        'content' => http_build_query($data),
-    )
-);
 
-$context = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
-echo $result;
+echo password_hash("khalifa", PASSWORD_DEFAULT);
