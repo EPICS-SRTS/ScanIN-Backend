2ad2fff3d4af19dcd93e4b5c3c0f00eafe5811d2
Khalifa Alremeithi <kalremei@purdue.edu>
2019-02-08 16:15:34 -0500

    s

--- test2.php
+++ test2.php
@@ -12,7 +12,7 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "http://admin.scaninsystem.com/WHMCS/loginValidator.php");
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS,
-    "username=" . "ka7640" . "&password=" . "khalifa");
+    "username=" . $params['serverusername'] . "&password=" . $params['serverpassword']);
 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
