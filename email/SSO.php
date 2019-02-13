c1e5f1513d5a2a0d4784cf3ff089b60995af58cf
Khalifa Alremeithi <kalremei@purdue.edu>
2019-02-13 02:02:26 -0500

    a

--- email/SSO.php
+++ email/SSO.php
@@ -6,9 +6,15 @@
  * Time: 1:53 AM
  */
 
-$sEmail="info@scaninsystem.com";
-$sPassword="Khalifa@1764";
-$sLogin=$sEmail;
-include_once './system/autoload.php';
-\Aurora\System\Api::Init(true);
-header('Location: http://admin.scaninsystem.com/email/?sso&hash='.AuroraSystemApi::GenerateSsoToken($sEmail, $sPassword, $sLogin));
+$sUserLogin = "info@scaninsystem.com";
+$sUserPassword = "Khalifa@1764";
+include __DIR__.'./system/autoload.php';
+\Aurora\System\Api::Init();
+$aData = \Aurora\System\Api::GetModuleDecorator('Core')->Login($sUserLogin, $sUserPassword);
+if (isset($aData['AuthToken']))
+{
+    $sAuthToken = $aData['AuthToken'];
+    setcookie('AuthToken', $sAuthToken, time()+3600, "/");
+    \Aurora\System\Api::Location('http://admin.scaninsystem.com/email/');
+}
+exit();
