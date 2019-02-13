6dc2f803280445479f816d1b7cf461ca71e6b9c5
Khalifa Alremeithi <kalremei@purdue.edu>
2019-02-13 01:55:21 -0500

    a

---
+++ email/SSO.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: khali
+ * Date: 2/13/2019
+ * Time: 1:53 AM
+ */
+
+$sEmail="info@scaninsystem.com";
+$sPassword="Khalifa@1764";
+$sLogin=$sEmail;
+include_once '/var/www/email/system/autoload.php';
+\Aurora\System\Api::Init(true);
+header('Location: http://admin.scaninsystem.com/email/?sso&hash='.AuroraSystemApi::GenerateSsoToken($sEmail, $sPassword, $sLogin));
