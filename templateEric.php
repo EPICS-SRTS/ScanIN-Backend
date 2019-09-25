4e63a2ce4e37d3d13bb6974421c9b3f08ad94292
ericpokeefe <okeefee@purdue.edu>
2019-09-25 09:11:23 -0400

    test change

--- templateEric.php
+++ templateEric.php
@@ -39,7 +39,7 @@ include "template/variables.php"; ?>
                 <div class="row">
                     <div class="col-xs-12">
                         <div class="page-title-box">
-                            <h4 class="page-title">Eric O'Keefe</h4>
+                            <h4 class="page-title">Eric O'Keefe is cool</h4>
                             <ol class="breadcrumb p-0 m-0">
                                 <li>
                                     <a href="#"><?php echo $Brand; ?></a>
